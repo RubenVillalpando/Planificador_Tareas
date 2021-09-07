f2c89dba398e38a68bc9b48d0ae14119ea9fa2d8
Ruben Villalpando Bremont <a01376331@itesm.mx>
2021-09-06 21:23:58 -0500

    implementar diseño de MVVM y activar los bindings

--- app/src/main/java/com/example/planificador_tareas/MainActivity.kt
+++ app/src/main/java/com/example/planificador_tareas/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.planificador_tareas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.planificador_tareas.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+
+        var mainView = mainBinding.root
+
+        setContentView(mainView)
+
+
     }
 }
