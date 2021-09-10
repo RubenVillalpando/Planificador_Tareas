2318bd39b395bc6a61c7f2ca25746c79ce8a72f1
Ruben Villalpando Bremont <a01376331@itesm.mx>
2021-09-10 11:18:56 -0500

    Aplicación terminada, falta arreglar un poco el código

--- app/src/main/java/com/example/planificador_tareas/view/MainActivity.kt
+++ app/src/main/java/com/example/planificador_tareas/view/MainActivity.kt
@@ -1,7 +1,8 @@
 package com.example.planificador_tareas.view
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
-import android.view.Gravity
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import com.example.planificador_tareas.databinding.ActivityMainBinding
@@ -11,54 +12,50 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
     lateinit var bindingFragmento: FragmentFragmentoNuevaTareaBinding
+    lateinit var spTitulos: SharedPreferences
+    lateinit var spCompletado: SharedPreferences
+    lateinit var manejadorTarea: Tarea
+    var sigId = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        spTitulos = getSharedPreferences("titulosTareas", Context.MODE_PRIVATE)
+        spCompletado = getSharedPreferences("boolsCompletado", Context.MODE_PRIVATE)
+        manejadorTarea = Tarea(spTitulos, spCompletado, this)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         bindingFragmento = FragmentFragmentoNuevaTareaBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        cargarTareas()
         registrarEventos()
+    }
+
+    private fun cargarTareas() {
+        val tareas = spTitulos.all
+        val checkboxes = spCompletado.all
+        var maxId= 0
+        for (llave in tareas.keys){
+            maxId = llave.toInt()
+            crearTarea(maxId, tareas.get(llave).toString(), checkboxes.get(llave) as Boolean )
+        }
+        sigId = maxId + 1
     }
 
     private fun registrarEventos() {
         binding.btnCrearTareaFragment.setOnClickListener {
             val nuevaTarea = FragmentoNuevaTarea{titulo->
-                crearTarea(titulo)}
-            nuevaTarea.show(supportFragmentManager, "creadorTarea")
-
-
+                crearTarea(sigId, titulo, false)
+            sigId++
+        }
+        nuevaTarea.show(supportFragmentManager, "creadorTarea")
         }
     }
 
-    private fun crearTarea(titulo: String) {
-        val root: ScrollView = binding.svContenedorTareas
-
-        //crear checkbox
-        val chkbox: CheckBox = CheckBox(this)
-
-
-        //crear textview
-        val tituloTarea: TextView = TextView(this)
-        tituloTarea.setText(titulo)
-        tituloTarea.width = 300
-        tituloTarea.gravity = Gravity.LEFT
-        tituloTarea.textSize = 20.0f
-
-
-        //crear boton eliminar
-        val botonEliminar: ImageButton = ImageButton(this)
-        botonEliminar.setImageResource(android.R.drawable.ic_menu_delete)
-
-        //crear el linearLayout
-        val nuevaTarea: LinearLayout = LinearLayout(this)
-        nuevaTarea.orientation = LinearLayout.HORIZONTAL
-        nuevaTarea.gravity = Gravity.LEFT
-
-        nuevaTarea.addView(chkbox)
-        nuevaTarea.addView(tituloTarea)
-        nuevaTarea.addView(botonEliminar)
+    private fun crearTarea(id: Int, titulo: String, completado: Boolean) {
+        val root: LinearLayout = binding.vlContenedorTarea
+        val nuevaTarea = manejadorTarea.crearTarea(id, titulo, completado)
         root.addView(nuevaTarea)
 
     }
---
+++ app/src/main/java/com/example/planificador_tareas/view/Tarea.kt
@@ -0,0 +1,90 @@
+package com.example.planificador_tareas.view
+
+import android.content.Context
+import android.content.SharedPreferences
+import android.util.DisplayMetrics
+import android.view.Gravity
+import android.view.View
+import android.widget.CheckBox
+import android.widget.ImageButton
+import android.widget.LinearLayout
+import android.widget.TextView
+
+
+class Tarea(spTitulos: SharedPreferences, spCompletado: SharedPreferences, context:Context) {
+
+
+    val editorTitulos = spTitulos.edit()
+    val editorCompletado = spCompletado.edit()
+    val context = context
+
+    private fun guardarTarea(id: Int, titulo: String, completado: Boolean) {
+        editorTitulos.putString(id.toString(), titulo)
+        editorTitulos.commit()
+        editorCompletado.putBoolean(id.toString(), completado)
+        editorCompletado.commit()
+    }
+
+    private fun editarGuardado(id: Int, checked: Boolean) {
+        editorCompletado.putBoolean(id.toString(), checked)
+        editorCompletado.commit()
+    }
+
+    private fun quitarTarea(id: Int){
+        editorTitulos.remove(id.toString())
+        editorTitulos.commit()
+        editorCompletado.remove(id.toString())
+        editorCompletado.commit()
+    }
+
+    fun crearTarea(id: Int, titulo: String, completado: Boolean): LinearLayout{
+
+        context.resources.displayMetrics
+
+        //crear checkbox
+        val chkbox= CheckBox(context)
+        chkbox.width = 48.toPx(context)
+        chkbox.height = 48.toPx(context)
+        chkbox.isChecked = completado
+
+
+        //crear textview
+        val tituloTarea= TextView(context)
+        tituloTarea.setText(titulo)
+        tituloTarea.width = 300.toPx(context)
+        tituloTarea.gravity = Gravity.LEFT
+        tituloTarea.textSize = 20.0f
+
+        //crear boton eliminar
+        val botonEliminar= ImageButton(context)
+        botonEliminar.setImageResource(android.R.drawable.ic_menu_delete)
+
+        //crear el linearLayout
+        val nuevaTarea = LinearLayout(context)
+        nuevaTarea.id = id
+        nuevaTarea.orientation = LinearLayout.HORIZONTAL
+        nuevaTarea.gravity = Gravity.LEFT
+
+        botonEliminar.setOnClickListener {
+            nuevaTarea.visibility = View.GONE
+            quitarTarea(nuevaTarea.id)
+        }
+
+        chkbox.setOnClickListener {
+            editarGuardado(nuevaTarea.id, chkbox.isChecked)
+        }
+
+        nuevaTarea.addView(chkbox)
+        nuevaTarea.addView(tituloTarea)
+        nuevaTarea.addView(botonEliminar)
+
+        guardarTarea(nuevaTarea.id, tituloTarea.text.toString(), chkbox.isChecked)
+
+        return nuevaTarea
+    }
+
+
+
+    private fun Int.toPx(context: Context) = this * context.resources.displayMetrics.densityDpi / DisplayMetrics.DENSITY_DEFAULT
+
+}
